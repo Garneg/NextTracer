1d5d7e902f05c31eab80a06dcaad976edbe040a4
Garneg <tisin371@gmail.com>
2022-05-06 22:49:37 +0300

    Добавьте файлы проекта.

---
+++ NextTracer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Silk.NET.Core;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+using Silk.NET.Maths;
+using Silk.NET.Vulkan;
+
+
+namespace NextTracer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var shit = Window.Create(new WindowOptions(
+                true,
+                Vector2D<int>.Zero,
+                new Vector2D<int>(640, 480),
+                300,
+                60,
+                GraphicsAPI.Default,
+                "Hello Silk.NET!",
+                WindowState.Normal,
+                WindowBorder.Resizable,
+                false,
+                true,
+                new VideoMode(300)));
+
+            //var shit = Window.Create(WindowOptions.Default);
+
+            shit.Render += elapsedTime =>
+            {
+                Console.WriteLine(1000 / (elapsedTime * 1000));
+            };
+
+            shit.Run();
+        }
+    }
+}
